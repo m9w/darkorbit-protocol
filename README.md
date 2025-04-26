f7bafbde7734f3bf363696723732a16f48419ede
m9w <52008918+m9w@users.noreply.github.com>
2025-04-26 06:21:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# darkorbit-protocol
+
+## Installation
+
+Add JitPack repository in your root `build.gradle.kts`:
+
+```kotlin
+repositories {
+    maven { url = uri("https://jitpack.io") }
+}
+
+dependencies {
+    implementation("com.github.m9w:darkorbit-protocol:latest")
+}
+```
---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+version = "0.0.1"
+group = "com.github.m9w"
+
+plugins { `maven-publish`; kotlin("jvm") version "2.1.20" }
+
+kotlin { jvmToolchain(17) }
+
+repositories { mavenCentral() }
+
+publishing {
+    java.withSourcesJar()
+
+    publications.create<MavenPublication>("maven") {
+        from(components["kotlin"])
+    }
+}
