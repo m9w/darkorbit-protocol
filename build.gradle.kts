b94ba10d641f1e59a3b9e346707e761a3201fffe
m9w <zc.besaba@gmail.com>
2025-05-12 04:10:08 +0300

    Update to 1.1.39
    Version hash: e0978404ac77a5751f514f0e07650fa9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-version = "1.1.38"
+version = "1.1.39"
 group = "com.github.m9w"
 
 plugins { `maven-publish`; kotlin("jvm") version "2.1.20" }
--- src/main/kotlin/com/darkorbit/AbsUserMessageInfoCommand.kt
+++ src/main/kotlin/com/darkorbit/AbsUserMessageInfoCommand.kt
@@ -1,11 +1,14 @@
 package com.darkorbit
 
 interface AbsUserMessageInfoCommand : ProtocolPacket {
+	var attachments: MutableList<LootModule>
 	var body: String
 	var boxId: Long
 	var insertDate: String
+	var isReceived: Int
 	var messageId: Int
 	var parent: Boolean
+	var replacements: String
 	var senderId: Int
 	var status: Int
 	var subject: String
